2ddf82fedcdba5368a69e566c4451331c37dcf4e
tkocmathla <matt.t.grimm@gmail.com>
2017-12-08 07:09:35 -0700

    Slow particle velocity over time; hard-code smoke color

--- src/particles/smoke.clj
+++ src/particles/smoke.clj
@@ -6,7 +6,6 @@
 (def rand-pos #(vector (+ 300 (rand (- (q/width) 600))) (q/height)))
 (def rand-speed #(max 0.1 (rand 3)))
 (def rand-life #(inc (rand-int 9)))
-(def rand-gray #(let [c (+ 5 (rand-int 10))] [c c c]))
 
 (defn old? [p] (-> p :life pos? not))
 
@@ -15,7 +14,6 @@
     {:pos (rand-pos)
      :velocity [(* speed (Math/cos (radians 90)))
                 (* speed (Math/sin (radians 90)) -1)] 
-     :color (rand-gray)
      :size 64
      :start-life start-life
      :life life
@@ -27,6 +25,7 @@
 (defn age-particle
   [{:keys [velocity life start-life] :as m}]
   (-> m
+      (update :velocity map* (repeat 0.997))
       (update :pos map+ velocity)
       (assoc :alpha (* 255 (/ life start-life)))
       (update :life #(- % (rand 0.07)))))
@@ -38,7 +37,7 @@
 (defn setup []
   (q/frame-rate 60)
   (q/blend-mode :add)
-  (reset! image (q/load-image "particleTexture16.png"))
+  (reset! image (q/load-image "particleTexture.png"))
   (q/resize @image 64 0)
   (repeatedly 1000 particle))
 
@@ -46,15 +45,14 @@
   (map (comp emit-particle age-particle) particles))
 
 (defn draw [particles]
-  (q/background 0 0 0)
+  (q/background 16 16 16)
   (q/no-stroke)
-  (doseq [{:keys [pos color alpha size life start-life]} (remove old? particles)] 
+  (doseq [{:keys [pos alpha size life start-life]} (remove old? particles)] 
     (q/push-matrix)
     (apply q/translate pos)
     (q/begin-shape)
     (q/texture @image)
-    (let [[r g b] color]
-      (q/tint r g b alpha))
+    (q/tint 8 8 8 alpha)
     (q/vertex 0 0 0 0)
     (q/vertex size 0 size 0)
     (q/vertex size size size size)
