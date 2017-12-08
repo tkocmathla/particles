173499b9c97a37f55032e26f3bb2707feb279dd5
tkocmathla <matt.t.grimm@gmail.com>
2017-12-07 21:03:57 -0700

    Almost realistic smoke

--- src/particles/smoke.clj
+++ src/particles/smoke.clj
@@ -5,28 +5,30 @@
 
 (def rand-pos #(vector (+ 300 (rand (- (q/width) 600))) (q/height)))
 (def rand-speed #(max 0.1 (rand 3)))
-(def rand-life #(inc (rand-int 8)))
-(def rand-gray #(let [c (+ 32 (rand-int 64))] [c c c (rand-int 256)]))
+(def rand-life #(inc (rand-int 9)))
+(def rand-gray #(let [c (+ 5 (rand-int 10))] [c c c]))
 
 (defn old? [p] (-> p :life pos? not))
 
 (defn particle []
-  (let [life (rand-life), speed (rand-speed)]
+  (let [start-life (rand-life), life (min start-life (rand-life)), speed (rand-speed)]
     {:pos (rand-pos)
      :velocity [(* speed (Math/cos (radians 90)))
                 (* speed (Math/sin (radians 90)) -1)] 
      :color (rand-gray)
-     :size [16 16]
-     :start-life life
-     :life life}))
+     :size 64
+     :start-life start-life
+     :life life
+     :alpha 255}))
 
 (defn emit-particle [p]
-  (cond-> p (old? p) (assoc :pos (rand-pos) :life (rand-life))))
+  (if (old? p) (particle) p))
 
 (defn age-particle
   [{:keys [velocity life start-life] :as m}]
   (-> m
       (update :pos map+ velocity)
+      (assoc :alpha (* 255 (/ life start-life)))
       (update :life #(- % (rand 0.07)))))
 
 ;; -----------------------------------------------------------------------------
@@ -37,6 +39,7 @@
   (q/frame-rate 60)
   (q/blend-mode :add)
   (reset! image (q/load-image "particleTexture16.png"))
+  (q/resize @image 64 0)
   (repeatedly 1000 particle))
 
 (defn step [particles]
@@ -45,17 +48,16 @@
 (defn draw [particles]
   (q/background 0 0 0)
   (q/no-stroke)
-  (doseq [{:keys [pos color size life start-life]} (remove old? particles)] 
+  (doseq [{:keys [pos color alpha size life start-life]} (remove old? particles)] 
     (q/push-matrix)
     (apply q/translate pos)
     (q/begin-shape)
     (q/texture @image)
     (let [[r g b] color]
-      (q/tint r g b (* 255 (/ life start-life))))
-    (let [[w h] size]
-      (q/vertex 0 0 0 0)
-      (q/vertex w 0 w 0)
-      (q/vertex w h w h)
-      (q/vertex 0 h 0 h))
+      (q/tint r g b alpha))
+    (q/vertex 0 0 0 0)
+    (q/vertex size 0 size 0)
+    (q/vertex size size size size)
+    (q/vertex 0 size 0 size)
     (q/end-shape :close)
     (q/pop-matrix)))
